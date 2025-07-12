35c03f9acf4c662f251716918faf8227df3035b1
ysrevan <yusiflirvan921@gmail.com>
2025-07-12 22:01:37 +0400

    project finish

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -2,13 +2,13 @@ import jwt from "jsonwebtoken";
 import "dotenv/config";
 
 export const generateToken = (id, res) => {
-  const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "2m" });
+  const token = jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "10m" });
 
   res.cookie("token", token, {
     httpOnly: true,
     secure: false,    
     sameSite: "lax",   
-    maxAge: 2 * 60 * 1000
+    maxAge: 10 * 60 * 1000
   })
 
   return token;
